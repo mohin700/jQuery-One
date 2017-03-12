ed47882fda6f03bffb872ee77e0a244eea241437
mohin <mymel@google.com>
2017-03-12 23:46:30 +0600

    Update toggle function

--- inc/min.js
+++ inc/min.js
@@ -5,4 +5,21 @@ $(document).ready(function(){
     $(".ui-close").click(function(){
         $(this).parent(".massege-block").fadeOut();
     });
+
+
+    //FAQ-Hide-Ans
+    $(".answer").hide();
+
+    //Toggle Function
+    $(".question-unit .question").click(function(){
+
+    	$(this).next('.answer').slideToggle();
+    })
+
+    //add question style
+    $('.question h3').addClass('fa-style');
+
+
+
+
 });
